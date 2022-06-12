b6dbaa277680bb2a5fb57c3a7105337f6677d307
marina <marina@example.com>
2022-06-12 18:28:25 +0300

    fixing jenkins script

--- jenkins-post-install.sh
+++ jenkins-post-install.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
 
-Jenkins_server="34.221.166.125"
-jenkins_agent="54.189.130.80"
+Jenkins_server="10.0.6.110"
+jenkins_agent="10.0.6.72"
+jenkins_key="terraform/keys/jenkins_key.pem"
+
+
 
 # run on server 
 
 echo "Configiring jenkins Server:"
 echo "##########################"
 echo 'download jenkins-cli.jar'
-ssh -i jenkins_ec2_key  ubuntu@$Jenkins_server "curl http://localhost:8080/jnlpJars/jenkins-cli.jar -o jenkins-cli.jar"
+ssh -i $jenkins_key   ubuntu@$Jenkins_server "curl http://localhost:8080/jnlpJars/jenkins-cli.jar -o jenkins-cli.jar"
 
 echo 'installing plugins'
-ssh -i jenkins_ec2_key  ubuntu@$Jenkins_server "echo 'installing plugins'; java -jar jenkins-cli.jar -s http://localhost:8080/ -webSocket install-plugin Git GitHub github-branch-source  pipeline-model-extensions build-monitor-plugin pipeline-build-step docker-workflow Swarm -deploy"
+ssh -i $jenkins_key  ubuntu@$Jenkins_server "echo 'installing plugins'; java -jar jenkins-cli.jar -s http://localhost:8080/ -webSocket install-plugin Git GitHub github-branch-source  pipeline-model-extensions build-monitor-plugin pipeline-build-step docker-workflow Swarm -deploy"
 
 
 
 # run on node:
-echo "Configiring jenkins Server:"
+echo "Configiring jenkins Node:"
 echo "##########################"
 echo "download node client"
-ssh -i jenkins_ec2_key  ubuntu@$jenkins_agent "curl http://$Jenkins_server:8080/swarm/swarm-client.jar -o swarm-client.jar"
+ssh -i $jenkins_key  ubuntu@$jenkins_agent "curl http://$Jenkins_server:8080/swarm/swarm-client.jar -o swarm-client.jar"
 
 echo "install cubectl"
-ssh -i jenkins_ec2_key  ubuntu@$jenkins_agent "sudo snap install kubectl --classic"
-ssh -i jenkins_ec2_key  ubuntu@$jenkins_agent "aws eks --region=us-west-2 update-kubeconfig --name mid-project-eks-cluster"
+ssh -i $jenkins_key  ubuntu@$jenkins_agent "sudo snap install kubectl --classic"
+ssh -i $jenkins_key  ubuntu@$jenkins_agent "aws eks --region=us-west-2 update-kubeconfig --name mid-project-eks-cluster"
 
 echo "install aws cli"
-ssh -i jenkins_ec2_key  ubuntu@$jenkins_agent "curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o awscliv2.zip "
-ssh -i jenkins_ec2_key  ubuntu@$jenkins_agent "unzip awscliv2.zip"
-ssh -i jenkins_ec2_key  ubuntu@$jenkins_agent "sudo ./aws/install"
+ssh -i $jenkins_key  ubuntu@$jenkins_agent "curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o awscliv2.zip "
+ssh -i $jenkins_key  ubuntu@$jenkins_agent "sudo apt install unzip; unzip awscliv2.zip"
+ssh -i $jenkins_key  ubuntu@$jenkins_agent "sudo ./aws/install"
 
 
 echo "connect node to Jenkins Server"
-ssh -i jenkins_ec2_key  ubuntu@$jenkins_agent "nohup java -jar swarm-client.jar -url http://$Jenkins_server:8080 -webSocket -name node1 -disableClientsUniqueId &"
+ssh -i $jenkins_key  ubuntu@$jenkins_agent "nohup java -jar swarm-client.jar -url http://$Jenkins_server:8080 -webSocket -name node1 -disableClientsUniqueId -retry 1 &"
 
-~
-~
