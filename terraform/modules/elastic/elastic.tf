4b8915b69f2349d171097b44f34a55d1c851f431
marina <marina@example.com>
2022-07-16 18:36:43 +0300

    after elastic

---
+++ terraform/modules/elastic/elastic.tf
@@ -0,0 +1,91 @@
+##################
+#Create elastic server
+##################
+
+resource "aws_instance" "elastic" {
+   ami                  = lookup(var.ami, var.region)
+  count                 = 1
+  instance_type         = "t3.medium"
+  key_name              = var.server_public_key
+  subnet_id             = element(var.subnet_id, count.index)
+  vpc_security_group_ids            = [aws_security_group.elastic_sg.id]
+  user_data = file("./modules/elastic/userdata.sh")
+  tags = {
+    Name = "Elastic Search Server"
+  }
+}
+
+#####################################
+#Create security group for elastic server
+#####################################
+
+resource "aws_security_group" "elastic_sg" {
+  name        = "elastic_sg"
+  description = "Allow straffic to elastic stack "
+   vpc_id = var.vpc_id
+  
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    description = "elasticsearch-rest-tcp"
+    /* self        = true */
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow ssh"
+  }
+
+  ingress {
+    from_port   = 9300
+    to_port     = 9300
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "elasticsearch-java-tcp"
+  }
+
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "elasticsearch-rest-tcp"
+  }
+
+
+    ingress {
+    from_port   = 5601
+    to_port     = 5601
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "kibana web interface"
+  }
+
+    
+    ingress {
+    from_port   = 5044
+    to_port     = 5044
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "logstash-tcp"
+  }
+
+  
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    description     = "Allow all outside security group"
+  }
+  tags = {
+    "name" = "elastic_sg"
+  }
+}
+
+
