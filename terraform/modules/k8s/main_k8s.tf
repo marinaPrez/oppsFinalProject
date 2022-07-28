fd5074e438cd2376340a213d0d33fb2ffc0c7f19
marina <marina@example.com>
2022-07-28 18:52:09 +0300

    adding auth

--- terraform/modules/k8s/main_k8s.tf
+++ terraform/modules/k8s/main_k8s.tf
@@ -99,9 +99,9 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
-
+###
 # add Jenkins IAM role to EKS cluster
-
+#####
 locals {
 map_roles = [
     {
